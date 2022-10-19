b02e40a78d29de9dd069eb8cd1fbc9e659075497
Mario Lugo <mlugo23699@gmail.com>
2022-10-19 14:58:58 -0700

    Finished views and urls files for journal_entries, done with backend

--- backend/journal_entries/urls.py
+++ backend/journal_entries/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('all/', views.journal_entries_list),
-    path('create/', views.create_entry),
+    path('create/', views.create_delete_entry),
+    path('edit/', views.draft_entry)
 ]
--- backend/journal_entries/views.py
+++ backend/journal_entries/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
@@ -15,9 +16,10 @@ def journal_entries_list(request):
     print(request)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
-@api_view(['GET', 'POST'])
-@permission_classes([AllowAny])
-def create_entry(request):
+@api_view(['GET', 'POST', 'DELETE'])
+@permission_classes([IsAuthenticated])
+def create_delete_entry(request):
+    entries = get_object_or_404(JournalEntries)
     if request.method == 'GET':
         entries = JournalEntries.objects.all(philosophile = request.philosophile)
         serializer = JournalEntriesSerializer(entries, many = True)
@@ -30,4 +32,21 @@ def create_entry(request):
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        entries.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
 
+@api_view(['GET', 'PUT'])
+@permission_classes([IsAuthenticated])
+def draft_entry(request, pk):
+    entries = get_object_or_404(JournalEntries, pk = pk)
+    if request.method == 'GET':
+        entries = JournalEntries.objects.all(philosophile = request.philosophile)
+        serializer = JournalEntriesSerializer(entries, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = JournalEntriesSerializer(entries, data = request.data)
+        if serializer.is_valid(raise_exception = True):
+            serializer.save(philosophile = request.philosophile)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
